2474cdb3e4436d2f2ef700a9c567553265c50551
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-28 22:31:21 -0300

    Add TypeScript and Tailwinds

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r from-red-200 to-red-700 h-screen ">
+      <h1 className="">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
